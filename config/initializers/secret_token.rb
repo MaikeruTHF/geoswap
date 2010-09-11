8df2555b680a103b8b6798be8a8339cb7d98b94f
mmoriya <mmoriya@camberhawaii.org>
2010-09-11 10:01:53 -1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geoswap::Application.config.secret_token = 'a89df307e88341d46694898894c0d3e57e66b27ae455f94693446dbfefcf35ea74d630f32a56b381e783128ea9ce558be9a8a63cc51939c32f68c4cbfe38e589'
